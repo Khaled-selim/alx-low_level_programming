691b80aca0551fcd0aab02b71fafb2e924109e82
Khaled-selim <khaled95selim@gmail.com>
2023-08-15 18:14:08 -0700

    fix 98

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,12 @@
 
 /**
  * main - adds two integers and returns the result
- * 
  * Return: sum of a and b
  */
 int add(int a, int b)
 {
-	int x;
 
-	x = a + b;
-
+	int x = a + b;
 	return (x);
+
 }
