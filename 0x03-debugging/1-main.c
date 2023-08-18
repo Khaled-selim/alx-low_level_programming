7c28eb02cec1e14192bd4845a79309edc9dae82f
Khaled-selim <khaled95selim@gmail.com>
2023-08-18 06:02:56 -0700

    fix

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 /**
 * main - causes an infinite loop
@@ -12,7 +13,8 @@ int main(void)
 
 	i = 0;
 
-	/* while (i < 10)
+	/**
+	*  while (i < 10)
 	*{
 	*       putchar(i);
 	*}
